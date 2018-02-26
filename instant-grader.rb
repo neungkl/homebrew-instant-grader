d8c9437182140b020f8e188ca68f0f742fbc58ff
Kosate Limpongsa <kosatelim@gmail.com>
2018-02-26 12:42:51 +0700

    🎉 Add brew formula

---
+++ instant-grader.rb
@@ -0,0 +1,11 @@
+class InstantGrader < Formula
+  desc "A tool for grading your code and test subject in single file. Compiling, splitting, and testing in single command."
+  homepage "https://github.com/neungkl/instant-grader/"
+  url "https://github.com/neungkl/instant-grader/archive/v1.0.tar.gz"
+  sha256 "6368a89ae3c87a895c0e1a096c70067f2cb77f825e195005c36c41dca8772673"
+
+  def install
+    system "make", "build"
+    bin.install_symlink libexec/"build/instant-grader"
+  end
+end
