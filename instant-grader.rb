406164764d156ddff330595234bbd6715a2f2aef
Kosate Limpongsa <kosatelim@gmail.com>
2018-02-26 14:14:03 +0700

    📦 Update formula

--- instant-grader.rb
+++ instant-grader.rb
@@ -1,11 +1,15 @@
 class InstantGrader < Formula
   desc "A tool for grading your code and test subject in single file. Compiling, splitting, and testing in single command."
   homepage "https://github.com/neungkl/instant-grader/"
-  url "https://github.com/neungkl/instant-grader/archive/v1.0.tar.gz"
-  sha256 "6368a89ae3c87a895c0e1a096c70067f2cb77f825e195005c36c41dca8772673"
+  url "https://github.com/neungkl/instant-grader/archive/v1.0.2.tar.gz"
+  sha256 "e318997ee93dff51ada7ee5e41d869b4dc8e070dc2cbef105a835159108b1f3f"
 
   def install
-    system "make", "build"
-    bin.install_symlink libexec/build/"instant-grader"
+    system "make build"
+    bin.install "build/instant-grader" => "instant-grader"
+  end
+
+  test do
+    system "make test"
   end
 end
